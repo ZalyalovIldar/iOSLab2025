05f22423652c24e4fbcf533960a9ca394df2359d
lajsan <lajsanminlebaeva@mail.ru>
2025-09-11 14:25:34 -0700

    initial

---
+++ Business Card/Business Card/Business_CardApp.swift
@@ -0,0 +1,17 @@
+//
+//  Business_CardApp.swift
+//  Business Card
+//
+//  Created by Ляйсан on 11.09.2025.
+//
+
+import SwiftUI
+
+@main
+struct Business_CardApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ Business Card/Business CardTests/Business_CardTests.swift
@@ -0,0 +1,17 @@
+//
+//  Business_CardTests.swift
+//  Business CardTests
+//
+//  Created by Ляйсан on 11.09.2025.
+//
+
+import Testing
+@testable import Business_Card
+
+struct Business_CardTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
