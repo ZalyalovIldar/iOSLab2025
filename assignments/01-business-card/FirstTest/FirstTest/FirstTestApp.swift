9a73c86d74e6438d4c128d7f81a7f28f0e3c38ee
Myrsus <130497768+Myrsus@users.noreply.github.com>
2025-09-15 18:53:17 +0300

    Add files via upload

---
+++ assignments/01-business-card/FirstTest/FirstTest/FirstTestApp.swift
@@ -0,0 +1,17 @@
+//
+//  FirstTestApp.swift
+//  FirstTest
+//
+//  Created by Myrsus on 09.09.2025.
+//
+
+import SwiftUI
+
+@main
+struct FirstTestApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
