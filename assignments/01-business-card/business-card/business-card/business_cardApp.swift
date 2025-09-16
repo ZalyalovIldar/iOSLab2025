785bb16930048f249895d10c27c361111a499b38
tensaid7 <tensaid7@gmail.com>
2025-09-16 15:56:55 +0300

    first homework

---
+++ assignments/01-business-card/business-card/business-card/business_cardApp.swift
@@ -0,0 +1,17 @@
+//
+//  business_cardApp.swift
+//  business-card
+//
+//  Created by Айнур on 09.09.2025.
+//
+
+import SwiftUI
+
+@main
+struct business_cardApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
