8057b03779ca957a03d6734fab0e277e0b4ae6e9
NAstyasasasas <142779148+NAstyasasasas@users.noreply.github.com>
2025-09-22 20:02:20 +0300

    task2

---
+++ assignments/02-guessIt/homework2App.swift
@@ -0,0 +1,17 @@
+//
+//  homework2App.swift
+//  homework2
+//
+//  Created by Анастасия on 21.09.2025.
+//
+
+import SwiftUI
+
+@main
+struct Homework2App: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
