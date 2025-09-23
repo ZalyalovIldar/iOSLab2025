0781380571267e11a7d5a54c73c37eba3ac2845f
zakirka <zakirovazamat0508@gmail.com>
2025-09-23 17:44:01 +0300

    first commit 2 homework

---
+++ GuessIT_zakir/GuessIT_zakirApp.swift
@@ -0,0 +1,17 @@
+//
+//  GuessIT_zakirApp.swift
+//  GuessIT_zakir
+//
+//  Created by Azamat Zakirov on 23.09.2025.
+//
+
+import SwiftUI
+
+@main
+struct GuessIT_zakirApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
