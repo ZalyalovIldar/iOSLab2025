4c49358d68be1ab51ca4f11bf08c8e606f119057
tensaid7 <tensaid7@gmail.com>
2025-09-23 19:02:58 +0300

    second homework

--- assignments/01-business-card/business-card/business-card/BusinessCardApp.swift
+++ assignments/01-business-card/business-card/business-card/BusinessCardApp.swift
@@ -8,7 +8,7 @@
 import SwiftUI
 
 @main
-struct business_cardApp: App {
+struct BusinessCardApp: App {
     var body: some Scene {
         WindowGroup {
             ContentView()
---
+++ assignments/02-guessIt/guessIt/guessIt/guessItApp.swift
@@ -0,0 +1,17 @@
+//
+//  guessItApp.swift
+//  guessIt
+//
+//  Created by Айнур on 23.09.2025.
+//
+
+import SwiftUI
+
+@main
+struct guessItApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
