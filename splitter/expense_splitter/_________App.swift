9d57472e990fa17f0d5d640fae3eb7698143cc78
overmann <nikitosin2005@gmail.com>
2025-09-30 21:46:44 +0300

    made task 3(splitter, Timofey filatchev)

---
+++ splitter/expense_splitter/_________App.swift
@@ -0,0 +1,14 @@
+//___FILEHEADER___
+
+import SwiftUI
+
+import SwiftUI
+
+@main
+struct ExpenseSplitterApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
