9f0a3a8c0d17552a7f19ec4e30af325ec590dcb2
NAstyasasasas <142779148+NAstyasasasas@users.noreply.github.com>
2025-09-30 12:13:00 +0300

    Task3

---
+++ assignments/03-expense-splitter/Homework3App.swift
@@ -0,0 +1,17 @@
+//
+//  Homework3App.swift
+//  Homework3
+//
+//  Created by Анастасия on 30.09.2025.
+//
+
+import SwiftUI
+
+@main
+struct Homework3App: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
