8ab44f234b5cd9447581333591ca0022e8172725
OstapMamykin <ostapmamykin@OstapMamykins-MacBook-Pro.local>
2025-10-07 21:48:05 +0300

    HW_4

---
+++ hw_4/hw_4Tests/hw_4Tests.swift
@@ -0,0 +1,17 @@
+//
+//  hw_4Tests.swift
+//  hw_4Tests
+//
+//  Created by OstapMamykin on 07.10.2025.
+//
+
+import Testing
+@testable import hw_4
+
+struct hw_4Tests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
