9252548793c3942c7a498d81d283dae6c8f65581
krnklvx <sawosit@gmail.com>
2025-10-10 02:01:31 +0300

    hw3

---
+++ hmwk_3/hmwk_3/ExpenseSplitterAppApp.swift
@@ -0,0 +1,18 @@
+//
+//  ExpenseSplitterAppApp.swift
+//  hmwk_3
+//
+//  Created by krnklvx on 10.10.2025.
+//
+
+
+import SwiftUI
+
+@main
+struct ExpenseSplitterAppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ hmwk_3/hmwk_3/Views/SummaryView.swift
@@ -0,0 +1,47 @@
+//
+//  SummaryView.swift
+//  hmwk_3
+//
+//  Created by krnklvx on 10.10.2025.
+//
+
+
+import SwiftUI
+
+struct SummaryView: View {
+    let total: Double
+    let average: Double
+    let personsCount: Int
+    
+    var body: some View {
+        VStack(alignment: .leading, spacing: 10) {
+            Text("Итого: ")
+                .font(.headline)
+                .padding(.bottom, 5)
+            
+            HStack {
+                Text("Общая сумма:")
+                Spacer()
+                Text("\(total, specifier: "%.2f")")
+                    .fontWeight(.medium)
+            }
+                        
+            HStack {
+                Text("Средняя сумма:")
+                Spacer()
+                Text("\(average, specifier: "%.2f")")
+                    .fontWeight(.medium)
+            }
+                        
+            HStack {
+                Text("Участников:")
+                Spacer()
+                Text("\(personsCount) чел.")
+                    .fontWeight(.medium)
+            }
+        }
+        .padding()
+        .background(Color.gray.opacity(0.1))
+        .cornerRadius(10)
+    }
+}
