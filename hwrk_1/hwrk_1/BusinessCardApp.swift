7d613cd7e02b7ba2c945acb6749146863f21f63f
krnklvx <sawosit@gmail.com>
2025-10-10 01:34:57 +0300

    HW1

---
+++ hwrk_1/hwrk_1/BusinessCardApp.swift
@@ -0,0 +1,30 @@
+//
+//  BusinessCardApp.swift
+//  
+//
+//  Created by krnklvx on 10.10.2025.
+//
+
+
+import SwiftUI
+
+
+@main
+struct BusinessCardApp: App {
+    var body: some Scene {
+        WindowGroup {
+            BusinessCardView()
+        }
+    }
+}
+
+struct BusinessCardViewApp_Previews: PreviewProvider {
+    static var previews: some View {
+        Group {
+            BusinessCardView()
+                .preferredColorScheme(.light) // светлая тема
+            BusinessCardView()
+                .preferredColorScheme(.dark)  // тёмная тема
+        }
+    }
+}
