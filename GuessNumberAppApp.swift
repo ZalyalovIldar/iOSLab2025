d1cb8ad5d8a283d7bc151afe8f528973486ba8c7
krnklvx <sawosit@gmail.com>
2025-10-10 00:35:21 +0300

    hw2

---
+++ GuessNumberAppApp.swift
@@ -0,0 +1,18 @@
+//
+//  GuessNumberAppApp.swift
+//  
+//
+//  Created by krnklvx on 10.10.2025.
+//
+
+import SwiftUI
+
+@main
+struct GuessNumberAppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
+
