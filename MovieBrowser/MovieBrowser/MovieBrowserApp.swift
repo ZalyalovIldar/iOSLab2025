0005f49ba1e58f04edd47f15a513ddd9bd170a06
lajsan <lajsanminlebaeva@mail.ru>
2025-12-05 15:51:57 -0800

    initial commit

---
+++ MovieBrowser/MovieBrowser/MovieBrowserApp.swift
@@ -0,0 +1,17 @@
+//
+//  MovieBrowserApp.swift
+//  MovieBrowser
+//
+//  Created by Ляйсан on 5/12/25.
+//
+
+import SwiftUI
+
+@main
+struct MovieBrowserApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
