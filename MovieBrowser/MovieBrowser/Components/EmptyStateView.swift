d543775103551e4282352ef3ff9584c8741e86ed
lajsan <lajsanminlebaeva@mail.ru>
2025-12-20 16:07:03 -0800

    style fix

---
+++ MovieBrowser/MovieBrowser/Components/EmptyStateView.swift
@@ -0,0 +1,47 @@
+//
+//  EmptyStateView.swift
+//  MovieBrowser
+//
+//  Created by Ляйсан on 15/12/25.
+//
+
+import SwiftUI
+
+struct EmptyStateView: View {
+    let title: String
+    let subTitle: String
+    let imageName: String
+    let buttonLabel: String
+    let action: () -> ()
+    
+    var body: some View {
+        VStack {
+            Image(systemName: imageName)
+                .font(.largeTitle)
+            Text(title)
+                .font(.title2.bold())
+                .foregroundStyle(.white)
+            Text(subTitle)
+                .font(.subheadline)
+                .foregroundStyle(.lightGrey)
+            
+            Button {
+                action()
+            } label: {
+                Text(buttonLabel)
+                    .font(.headline)
+                    .padding(.horizontal, 10)
+                    .padding(.vertical, 5)
+            }
+            .buttonStyle(.glassProminent)
+            .tint(.lightVine)
+            .padding(10)
+
+        }
+        
+    }
+}
+
+#Preview {
+    EmptyStateView(title: "No Movies", subTitle: "Tap plus button to add a movie", imageName: "popcorn", buttonLabel: "No",action: {})
+}
