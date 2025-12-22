2d608483376ed4fafa6fde6c75d38312364cd241
lajsan <lajsanminlebaeva@mail.ru>
2025-12-21 20:02:33 -0800

    initial commit

---
+++ CryptoList/CryptoList/CryptoListApp.swift
@@ -0,0 +1,17 @@
+//
+//  CryptoListApp.swift
+//  CryptoList
+//
+//  Created by Ляйсан on 21/12/25.
+//
+
+import SwiftUI
+
+@main
+struct CryptoListApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
