4afba585012b3b07a7f99752eebddc85ea977910
ailinnnnn <ailinyashka75@gmail.com>
2025-12-30 12:11:24 +0300

    base level

---
+++ assignments/04-note-mvvm/notee/notee/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  notee
+//
+//  Created by lunnn on 29.12.2025.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ assignments/04-note-mvvm/notee/notee/noteeApp.swift
@@ -0,0 +1,17 @@
+//
+//  noteeApp.swift
+//  notee
+//
+//  Created by lunnn on 29.12.2025.
+//
+
+import SwiftUI
+
+@main
+struct noteeApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
