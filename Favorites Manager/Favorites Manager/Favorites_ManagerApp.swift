2b913c1691b662e1309fc3732e0ded63f5906e25
zakirka <zakirovazamat0508@gmail.com>
2025-12-31 04:31:15 +0300

    add hw-9

---
+++ Favorites Manager/Favorites Manager/Favorites_ManagerApp.swift
@@ -0,0 +1,17 @@
+//
+//  Favorites_ManagerApp.swift
+//  Favorites Manager
+//
+//  Created by Azamat Zakirov on 31.12.2025.
+//
+
+import SwiftUI
+
+@main
+struct Favorites_ManagerApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
